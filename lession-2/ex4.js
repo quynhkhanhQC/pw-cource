06bbeea9bfb05abd5881f217f69e91d9bddb5b10
K9 - Khanh <quynhkhanh152@gmail.com>
2024-08-26 22:25:04 +0700

    feat: add ex lession-2

---
+++ lession-2/ex4.js
@@ -0,0 +1,8 @@
+let height = 160;
+let maximumHeight = height % 100;
+let idealHeight = (height*9)/10;
+let minimumHeight = (height*8)/10;
+console.log("Cân nặng tối đa: ", maximumHeight);
+console.log("Cân nặng lý tưởng: ", idealHeight);
+console.log("Cân nặng tối thiểu: ", minimumHeight);
+
