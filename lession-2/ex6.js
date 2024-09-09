20742f5138b340a5a94f6a3c41ec1ea9e0018a34
K9 - Khanh <quynhkhanh152@gmail.com>
2024-09-09 22:46:17 +0700

    chore: delete lesson 2

--- lession-2/ex6.js
+++
@@ -1,4 +0,0 @@
-for(let i = 12; i <= 30; i++){
-    console.log("In ra giá trị từ 12 đến 30", i)
-
-}
--- lession-2/ex7.js
+++
@@ -1,5 +0,0 @@
-for(let i = 1000; i <= 2000; i++){
-    if(i % 3 == 0){
-        console.log(`Số ${i} là số chia hết cho 3`);
-    }
-}
